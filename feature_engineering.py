9a955fe7e3b1039f2bda5a240699abf295585b8d
Jean Duffy <jean.duffy@hotmail.com>
2024-02-22 15:12:42 +0100

    Property type split

--- feature_engineering.py
+++ feature_engineering.py
@@ -11,40 +11,65 @@ class Feature_engineering():
     def f_engineering(self):
         """Carries out ."""
         # Load the data
-        df = pd.read_csv("data/properties.csv")
+        data = pd.read_csv("data/properties.csv")
 
         #--------------------------------------------------------------
         # Preprocessing
         #--------------------------------------------------------------
         
         # Filling in missing longitude and latitude values with the average value for that locality
-        for locality in df.locality.unique():
-            mean_lat = round(df[df['locality'] == locality].latitude.mean(), 6)
-            mean_long = round(df[df['locality'] == locality].longitude.mean(), 6)
+        for locality in data.locality.unique():
+            mean_lat = round(data[data['locality'] == locality].latitude.mean(), 6)
+            mean_long = round(data[data['locality'] == locality].longitude.mean(), 6)
 
             #print(f'{locality}\n  mean latitude: {mean_lat}\n  mean longitude: {mean_long}')
             
             # Condition
-            cond = df['locality'] == locality
+            cond = data['locality'] == locality
 
             # Replacing the empt values with means
-            df.loc[cond,'latitude'] = df.loc[cond,'latitude'].fillna(mean_lat)
-            df.loc[cond,'longitude'] = df.loc[cond,'longitude'].fillna(mean_long)
+            data.loc[cond,'latitude'] = data.loc[cond,'latitude'].fillna(mean_lat)
+            data.loc[cond,'longitude'] = data.loc[cond,'longitude'].fillna(mean_long)
         
-        df.drop(df[df['subproperty_type'] == "CASTLE"].index)
+        data.drop(data[data['subproperty_type'] == "CASTLE"].index)
+        data = data.drop(data[(data["epc"] == "A++") & (data["region"] == "Brussels-Capital")].index)
 
         # Removing outliers from numerical columns based on the z-score
-        
-        for column in df.select_dtypes(include=["float64"]).columns:
-            z = np.abs(zscore(df[column]))
+        for column in data.select_dtypes(include=["float64"]).columns:
+            z_house = np.abs(zscore(data.loc[data["property_type"] == "HOUSE", column]))
+            z_apartment = np.abs(zscore(data.loc[data["property_type"] == "APARTMENT", column]))
 
             # Identify outliers with a z-score greater than 3
             threshold = 3
-            outliers = df[z > threshold]
+            z = pd.concat([z_house, z_apartment]).sort_index()
+            outliers = data[z > threshold]
 
-            df = df.drop(outliers.index) # Drop outliers
+            # Print the outliers
+            #print(f"{column}: {len(outliers)}")
+            data = data.drop(outliers.index)
+        '''
+        data['epc_flanders'] = data[data['region'] == "Flanders"]["epc"]
+        data['epc_wallonia'] = data[data['region'] == "Wallonia"]["epc"]
+        data['epc_brussels'] = data[data['region'] == "Brussels-Capital"]["epc"]
+        data["epc_flanders"].fillna(value='None', inplace=True)
+        data["epc_wallonia"].fillna(value='None', inplace=True)
+        data["epc_brussels"].fillna(value='None', inplace=True)
+        '''
+        
+        # Remap equipped_kitchen to lessen the n. categories
+        # Define map
+        kitchen_map = {
+            'HYPER_EQUIPPED': 'INSTALLED',
+            'SEMI_EQUIPPED': 'INSTALLED',
+            'USA_HYPER_EQUIPPED': 'INSTALLED',
+            'USA_INSTALLED': 'INSTALLED',
+            'USA_SEMI_EQUIPPED': 'INSTALLED',
+            'USA_UNINSTALLED': 'NOT_INSTALLED',
+        }
+        # Remap
+        data["equipped_kitchen_short"] = data["equipped_kitchen"].map(kitchen_map)
         
         #--------------------------------------------------------------
         # Output
         #--------------------------------------------------------------
-        df.to_csv("data/input.csv", index=False)
+        data.to_csv("data/input.csv", index=False)
